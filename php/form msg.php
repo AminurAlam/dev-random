0a8183a4ff6e079e6e9d3e7a209bdf9c2b8568a4
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-04-28 20:20:11 +0530

    php: new lang

---
+++ php/form msg.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+<h2>Enter Two Numbers</h2>
+
+<form method="POST">
+    Number 1: <input type="number" name="num1" required><br><br>
+    Number 2: <input type="number" name="num2" required><br><br>
+    <input type="submit" name="submit" value="Calculate">
+</form>
+
+
+<?php
+/*
+php goes here 
+*/
+?>
+
+</body>
+</html>
