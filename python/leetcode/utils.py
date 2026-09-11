d561c902fb7c9dff5bf6ed7f7668fa80e2c92a4d
AminurAlam <oscorp2004@gmail.com>
2026-09-11 11:04:46 +0530

    python(leetcode): split into files

--- python/leetcode/utils.py
+++ python/leetcode/utils.py
@@ -1,3 +1,4 @@
+from types import GeneratorType
 from typing import List, Optional
 import math
 import sys
@@ -10,12 +11,31 @@ class ListNode:
         self.val = val
         self.next = next
 
+    def __repr__(self) -> str:
+        return str(list(self.__iter__()))
+
+    def __iter__(self) -> GeneratorType:
+        curNode = self
+        while curNode:
+            yield curNode.val
+            curNode = curNode.next
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+    def __iter__(self):
+        queue = [self]
+        while queue:
+            node = queue.pop(0)
+            yield node.val
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
 
 
 def test(func, cases: list[tuple]):
